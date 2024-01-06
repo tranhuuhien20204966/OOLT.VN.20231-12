bc3f9e60ee93b222d0d66350dc69b3663a78f68d
GHCMaxim <101856461+GHCMaxim@users.noreply.github.com>
2024-01-06 16:46:24 +0700

    feat(Timer): Added timer to Start, Continue, Pause, Replay

--- sourcecode/cellDivision/src/controller/contentLayerController.java
+++ sourcecode/cellDivision/src/controller/contentLayerController.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Label;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,60 +26,61 @@ import javafx.stage.Stage;
 
 public class contentLayerController {
 
+    Timer timer = new Timer();
+    private boolean started = false;
     @FXML
     private Button BackButton;
-    
+
     @FXML
     private ImageView detailCell;
-    
+
     @FXML
     private ImageView CellDivision;
-    
+
     @FXML
     private Button nextbutton;
 
     @FXML
     private Button previousButton;
-    
+
     @FXML
-    private VBox Vbox ;
-   
+    private VBox Vbox;
+
     private int currentPhase = 0;
-    
+
     private ArrayList<Image> cellDivisionProcessImage = new ArrayList<Image>();
-    
+
     @FXML
     private ProgressBar processBar;
-    
-    double progress ;
-    
+
+    double progress;
+
     @FXML
     void initialize() {
-    	processBar.setStyle("-fx-accent : #58b7c5");
-    	
+        processBar.setStyle("-fx-accent : #58b7c5");
+
     }
-    
-    
+
     @FXML
     void goBack(ActionEvent event) throws IOException {
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/main.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/screen/main.fxml"));
         Parent root = loader.load();
-    	Scene scene = new Scene(root);
-//    	helpLayerController helpController = loader.getController();
-//    	helpController.goBack(event);
+        Scene scene = new Scene(root);
+        // helpLayerController helpController = loader.getController();
+        // helpController.goBack(event);
         Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
-    	stage.show();
+        stage.show();
     }
-    
+
     public void setImage(String imagePath, Image firstPhase) {
         Image image = new Image(imagePath);
         detailCell.setImage(image);
         CellDivision.setImage(firstPhase);
     }
-    
+
     public void setContentCell(Object obj) {
-    	Class<?> clazz = obj.getClass();
+        Class<?> clazz = obj.getClass();
         // Lấy tất cả các trường của lớp
         Field[] fields = clazz.getDeclaredFields();
         ObservableList<String> fieldData = FXCollections.observableArrayList();
@@ -91,72 +94,91 @@ public class contentLayerController {
             } catch (IllegalAccessException e) {
                 value = "Cannot access the field";
             }
-      
+
             fieldData.add(fieldName + " :" + value);
             Label label = new Label(fieldName + " :" + value);
             Vbox.getChildren().add(label);
-            
+
         }
-        
+
     }
-    
+
     @FXML
     void showNextPhase(ActionEvent event) {
-    	if(currentPhase < cellDivisionProcessImage.size() - 1) {
-        CellDivision.setImage(cellDivisionProcessImage.get(currentPhase + 1));
-        currentPhase = currentPhase + 1;
-        progress =(double) currentPhase/(cellDivisionProcessImage.size()-2);
-        processBar.setProgress(progress);
-    	}
+        if (currentPhase < cellDivisionProcessImage.size() - 1) {
+            CellDivision.setImage(cellDivisionProcessImage.get(currentPhase + 1));
+            currentPhase = currentPhase + 1;
+            progress = (double) currentPhase / (cellDivisionProcessImage.size() - 2);
+            processBar.setProgress(progress);
+        }
     }
 
     @FXML
     void showPreviousPhase(ActionEvent event) {
-    	if(currentPhase > 0) {
-    		CellDivision.setImage(cellDivisionProcessImage.get(currentPhase - 1));
-    		currentPhase = currentPhase - 1;
-    		progress = (double)currentPhase/(cellDivisionProcessImage.size()-2);
-    		processBar.setProgress(progress);
-    	}
+        if (currentPhase > 0) {
+            CellDivision.setImage(cellDivisionProcessImage.get(currentPhase - 1));
+            currentPhase = currentPhase - 1;
+            progress = (double) currentPhase / (cellDivisionProcessImage.size() - 2);
+            processBar.setProgress(progress);
+        }
     }
-    
-    public void loadImageArray(ArrayList<Image> ImageArray ) {
-    	for(Image image : ImageArray) {
-    		cellDivisionProcessImage.add(image);
-    	}
+
+    public void loadImageArray(ArrayList<Image> ImageArray) {
+        for (Image image : ImageArray) {
+            cellDivisionProcessImage.add(image);
+        }
     }
-    // Em làm cho anh thêm chức năng này nha kiểu anh có 1 mảng ảnh cellDivisionProcessImage và muốn hiển thị mỗi
-    // ảnh lần lượt vào CellDivision thì xử lý các nút còn lại
-	//    	Start : khi nhấn sẽ trở về ảnh đầu trong mảng cellDivisionProcessImage sau đó màn cellDivison sẽ tự động next
-	//    	ảnh tầm 2,3s gì đấy cho đến ảnh cuối trong mảng cellDivisionProcessImage
-	//    	Continue : khi nhấn sẽ lấy phaseCurrent và auto chạy tiếp
-	//    	Pause : Lấy phasecurrent và hiển thị ảnh của phase đó
-	//    	replay : giống start tuy nhiên điều kiện phaseCurrent là phase cuối
-    	
-    
     @FXML
-    void continueProcess(ActionEvent event) {
-
+    void startProcess(ActionEvent event) {
+        started = true;
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (currentPhase == cellDivisionProcessImage.size() - 1) {
+                    timer.cancel();
+                } else {
+                    showNextPhase(event);
+                }
+            }
+        }, 1000, 3000);
     }
-
 
     @FXML
     void pauseProcess(ActionEvent event) {
+        timer.cancel();
+    }
 
+    @FXML
+    void continueProcess(ActionEvent event) {
+        if (started) {
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (currentPhase == cellDivisionProcessImage.size() - 1) {
+                        timer.cancel();
+                    } else {
+                        showNextPhase(event);
+                    }
+                }
+            }, 1000, 3000);
+        }
     }
 
     @FXML
     void replayProcess(ActionEvent event) {
-
+        if (currentPhase == cellDivisionProcessImage.size() - 1) {
+            currentPhase = 0;
+            CellDivision.setImage(cellDivisionProcessImage.get(currentPhase));
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (currentPhase == cellDivisionProcessImage.size() - 1) {
+                        timer.cancel();
+                    } else {
+                        showNextPhase(event);
+                    }
+                }
+            }, 1000, 3000);
+        }
     }
-
-
-    @FXML
-    void startProcess(ActionEvent event) {
-
-    }
-
-
 }
-   
-
